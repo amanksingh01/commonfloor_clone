517615a5a6cc6e1c24ec64b2503c4d971328b3c9
Aman Kumar Singh <amanksingh01@gmail.com>
2019-02-22 00:34:44 +0530

    Add Properties index and show pages

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,2 +1,10 @@
 class PropertiesController < ApplicationController
+
+  def index
+    @properties = Property.paginate(page: params[:page], per_page: 12)
+  end
+
+  def show
+    @property = Property.find(params[:id])
+  end
 end
