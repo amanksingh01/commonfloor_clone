f32f41eb87aafffaa3e57b721460e9de340c270d
Aman Kumar Singh <amanksingh01@gmail.com>
2019-02-23 00:33:38 +0530

    Add tests for the Property index and show pages

---
+++ test/integration/properties_index_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class PropertiesIndexTest < ActionDispatch::IntegrationTest
+  include ActionView::Helpers::DateHelper
+  
+  test "properties index including pagination links" do
+    get properties_path
+    assert_template 'properties/index'
+    assert_select 'nav.pagination', count: 1
+    first_page_of_properties = Property.paginate(page: 1, per_page: 12)
+    first_page_of_properties.each do |property|
+      assert_match  (property.bed_rooms == 'na' ? property.area.to_s :
+                                                  property.bed_rooms.upcase),
+                    response.body
+      assert_match  property.property_type.capitalize,      response.body
+      assert_match  property.property_status,               response.body
+      assert_match  property.locality.titleize,             response.body
+      assert_match  property.city.titleize,                 response.body
+      assert_match  property.price,                         response.body
+      assert_match  time_ago_in_words(property.created_at), response.body
+      assert_select 'a[href=?]', property_path(property), text: 'View details'
+    end
+  end
+end
---
+++ test/integration/properties_show_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class PropertiesShowTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @property = properties(:dum_dum)
+  end
+
+  test "property display" do
+    user = @property.user
+    log_in_as(user)
+    assert is_logged_in?
+    get property_path(@property)
+    assert_template 'properties/show'
+    # Property details
+    assert_match @property.owner_name,                 response.body
+    assert_match @property.property_type.capitalize,   response.body
+    assert_match @property.property_status.capitalize, response.body
+    assert_match @property.bed_rooms.upcase,           response.body
+    assert_match @property.area,                       response.body
+    assert_match @property.price,                      response.body
+    assert_match @property.street_address.titleize,    response.body
+    assert_match @property.locality.titleize,          response.body
+    assert_match @property.city.titleize,              response.body
+    assert_match @property.state.titleize,             response.body
+    assert_match @property.pincode,                    response.body
+    assert_match @property.country.titleize,           response.body
+    # Users contact deails
+    assert_select 'a[href=?]', user_path(user), text: user.name
+    assert_match  user.mobile_number, response.body
+    # Links to modify/delete property
+    assert_select 'a[href=?]', edit_property_path(@property),
+                               text: 'Modify property details'
+    assert_select 'a[href=?]', property_path(@property), text: 'Delete property'
+  end 
+end
