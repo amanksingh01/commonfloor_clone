24dc35cf5e41bd5149fe8ffca9be7fb74c449ee6
Aman Kumar Singh <amanksingh01@gmail.com>
2019-02-26 12:48:22 +0530

    Add Properties access control

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,4 +1,5 @@
 class PropertiesController < ApplicationController
+  before_action :logged_in_user, except: :index
 
   def index
     @properties = Property.paginate(page: params[:page], per_page: 12)
@@ -6,5 +7,20 @@ class PropertiesController < ApplicationController
 
   def show
     @property = Property.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
--- test/controllers/properties_controller_test.rb
+++ test/controllers/properties_controller_test.rb
@@ -1,7 +1,82 @@
 require 'test_helper'
 
 class PropertiesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user     = users(:aman)
+    @property = properties(:dum_dum)
+  end
+
+  test "should redirect show when not logged in" do
+    get property_path(@property)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect new when not logged in" do
+    get new_property_path
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should get new when logged in" do
+    log_in_as(@user)
+    get new_property_path
+    assert_response :success
+  end
+
+  test "should redirect create when not logged in" do
+    assert_no_difference 'Property.count' do
+      post properties_path, params: { property: {
+                                        owner_name:      "ajay sharma",
+                                        property_type:   "apartment",
+                                        property_status: "sell",
+                                        bed_rooms:       "3bhk",
+                                        area:            "1500",
+                                        price:           "5000000",
+                                        street_address:  "121, dum dum road",
+                                        locality:        "dum dum",
+                                        city:            "kolkata",
+                                        state:           "west bengal",
+                                        pincode:         "700074",
+                                        country:         "india" } }
+    end
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_property_path(@property)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    assert_no_difference 'Property.count' do
+      patch property_path(@property), params: {
+                                        property: {
+                                          owner_name:      "ajay sharma",
+                                          property_type:   "apartment",
+                                          property_status: "sell",
+                                          bed_rooms:       "3bhk",
+                                          area:            "1500",
+                                          price:           "5000000",
+                                          street_address:  "121, dum dum road",
+                                          locality:        "dum dum",
+                                          city:            "kolkata",
+                                          state:           "west bengal",
+                                          pincode:         "700074",
+                                          country:         "india" } }
+    end
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'Property.count' do
+      delete property_path(@property)
+    end
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
 end
