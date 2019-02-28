bb645760c9a859a8a1320fbe84db4f6b81423aff
Aman Kumar Singh <amanksingh01@gmail.com>
2019-02-28 08:30:52 +0530

    feat: Allow user to delete Property

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,5 +1,6 @@
 class PropertiesController < ApplicationController
   before_action :logged_in_user, except: :index
+  before_action :correct_user,   only:   :destroy
 
   def index
     @properties = Property.paginate(page: params[:page], per_page: 12)
@@ -30,6 +31,9 @@ class PropertiesController < ApplicationController
   end
 
   def destroy
+    @property.destroy
+    flash[:success] = "Property deleted"
+    redirect_to current_user
   end
 
   private
@@ -39,5 +43,10 @@ class PropertiesController < ApplicationController
                                        :property_status, :bed_rooms, :area,
                                        :price, :street_address, :locality,
                                        :city, :state, :pincode, :country)
+    end
+
+    def correct_user
+      @property = current_user.properties.find_by(id: params[:id])
+      redirect_to root_url if @property.nil?
     end
 end
