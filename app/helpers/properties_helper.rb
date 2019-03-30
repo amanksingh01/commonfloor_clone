0ed477bd72af1043e3490e0bed76b44228d6ec1b
Aman Kumar Singh <amanksingh01@gmail.com>
2019-03-30 08:18:25 +0530

    refactor: Modify full title helper for properties

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -2,7 +2,8 @@ module PropertiesHelper
 
   # Returns the property title for the given property.
   def property_title(property)
-    (property.bed_rooms == 'na' ? (property.area.to_s + " sq. ft.") :
+    (property.bed_rooms == 'na' || property.property_type == "plot" ?
+                                   property.area.to_s + " sq. ft."  :
                                    property.bed_rooms.upcase) + " " +
     property.property_type.capitalize + " for " +
     property.property_status.capitalize + " in " +
