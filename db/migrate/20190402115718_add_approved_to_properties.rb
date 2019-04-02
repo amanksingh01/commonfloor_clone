46241d8433bcf7bc764539f460ea73e3d4853dd8
Aman Kumar Singh <amanksingh01@gmail.com>
2019-04-02 17:35:48 +0530

    fix: Property approved migration

--- db/migrate/20190402115718_add_approved_to_properties.rb
+++ db/migrate/20190402115718_add_approved_to_properties.rb
@@ -2,6 +2,6 @@ class AddApprovedToProperties < ActiveRecord::Migration[5.1]
   def change
     add_column :properties, :approved, :boolean, default: false
     add_column :properties, :approved_at, :datetime
-    add_reference :properties, :approved_by, foreign_key: true
+    add_reference :properties, :approved_by, foreign_key: { to_table: :users }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190401094206) do
+ActiveRecord::Schema.define(version: 20190402115718) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "property_id"
